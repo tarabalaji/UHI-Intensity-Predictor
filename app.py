b56e5c8f976bc64e6598a0205248bb843d2a1cee
tbalaji29 <tbalaji29@ndsj.org>
2026-07-10 14:26:04 -0700

    removed duplicate ML sensitivity

--- app.py
+++ app.py
@@ -366,12 +366,7 @@ fig_perm = px.bar(
     title="Permutation Importance of Features",
     text_auto=".3f",
 )
-
 st.plotly_chart(fig_perm)
-st.write("ML sensitivity output:")
-st.dataframe(
-    pd.DataFrame({"NDVI": ndvi_range[:10], "UHI": predicted_ml_sensitivity[:10]})
-)
 
 start = st.session_state.get("start_time")
 if start:
